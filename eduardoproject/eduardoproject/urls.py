15adcacb6b2114328c1f9805b7dfbf63a6fb842b
david.dasilva1 <david.dasilva@he-arc.ch>
2020-02-19 19:15:30 +0100

    add login view

--- eduardoproject/eduardoproject/urls.py
+++ eduardoproject/eduardoproject/urls.py
@@ -20,4 +20,5 @@ from register import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/",v.register,name="register"),
+    path("",include('django.contrib.auth.urls')),
 ]
