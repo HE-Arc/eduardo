695edcc0561bf5de8abe6859a9d60414c8d6687a
david.dasilva1 <david.dasilva@he-arc.ch>
2020-02-19 19:00:00 +0100

    add register form

--- eduardoproject/eduardoproject/urls.py
+++ eduardoproject/eduardoproject/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("eduardoApp.urls")),
+    path("register/",v.register,name="register"),
 ]
