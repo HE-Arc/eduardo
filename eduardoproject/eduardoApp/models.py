5e35976abdea81e15501434cba98ba9e6354896d
Sébastien Peiris <sebastien.peiris@he-arc.ch>
2020-03-03 09:43:46 +0100

    Article foreign key on user -> seller

--- eduardoproject/eduardoApp/models.py
+++ eduardoproject/eduardoApp/models.py
@@ -1,15 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class User(models.Model):
-    user_name = models.CharField(max_length=200)
-    user_email = models.EmailField(max_length=200)
-    user_password = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.user_name
-
 
 class Article(models.Model):
     article_name = models.CharField(max_length=200)
@@ -17,7 +10,11 @@ class Article(models.Model):
     quantity = models.IntegerField(default=1)
     state = models.CharField(max_length=50)
     color = models.CharField(max_length=100)
-    # seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    seller = models.ForeignKey(
+        User,
+        null = True,
+        on_delete = models.CASCADE,
+    )
 
     def __str__(self):
         return self.article_name
