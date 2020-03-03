a8104e4ce84aa122abdd0e436ac87bf5bbe04ebd
Sébastien Peiris <sebastien.peiris@he-arc.ch>
2020-03-03 10:25:39 +0100

    minor

--- eduardoproject/eduardoApp/models.py
+++ eduardoproject/eduardoApp/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.category_name
+
 class Article(models.Model):
     article_name = models.CharField(max_length=200)
     price = models.FloatField()
@@ -15,7 +21,11 @@ class Article(models.Model):
         null = True,
         on_delete = models.CASCADE,
     )
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.article_name
 
+
+
+    
