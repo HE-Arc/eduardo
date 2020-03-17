22a147d8210a6f307d92d6f25282553feb2842fc
David da Silva <david.dasilva@he-arc.ch>
2020-03-17 16:17:35 +0100

    add template header for every page

--- eduardoproject/eduardoproject/settings.py
+++ eduardoproject/eduardoproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'eduardoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'eduardoApp/partials/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
