f5bc5151d0ca848ce7f917bdcfbe7ba646a641f5
sebastie.peiris@he-arc <sebastien.peiris@he-arc.ch>
2020-03-17 09:16:23 +0100

    modif models

--- eduardoproject/eduardoApp/models.py
+++ eduardoproject/eduardoApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# --------------- User -----------------
 class User(models.Model):
     user_name = models.CharField(max_length=200)
     user_email = models.EmailField(max_length=200)
@@ -10,6 +11,13 @@ class User(models.Model):
     def __str__(self):
         return self.user_name
 
+# ---------------- Articles ---------------
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     article_name = models.CharField(max_length=200)
@@ -17,6 +25,9 @@ class Article(models.Model):
     quantity = models.IntegerField(default=1)
     state = models.CharField(max_length=50)
     color = models.CharField(max_length=100)
+    detail_text = models.TextField(max_length=600, null=True)
+    article_image = models.ImageField(upload_to="eduardoApp/static/images", null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     # seller = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
