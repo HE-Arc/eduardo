3049fc38b2f7cafe34e2d1e397208d4aa1c189e9
David da Silva <david.dasilva@he-arc.ch>
2020-04-19 17:23:36 +0200

    change to mysql database

--- eduardoproject/eduardoproject/settings.py
+++ eduardoproject/eduardoproject/settings.py
@@ -77,10 +77,17 @@ WSGI_APPLICATION = 'eduardoproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'eduardodb',
+    'USER': os.environ.get('GROUPNAME', 'root'),
+    'PASSWORD': os.environ.get('PASSWORD', 'admin'),
+    'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
+    'PORT': os.environ.get('MYSQL_PORT', '3306'),
+    'OPTIONS': {
+      'charset': 'utf8mb4'
     }
+  }
 }
 
 
